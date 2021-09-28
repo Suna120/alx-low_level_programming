f2b0fb22b0a2bc9d03981edfc307cbb5231360c9
SOnatti <90867768+SOnatti@users.noreply.github.com>
2021-09-28 16:59:45 +0200

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,8 +1,13 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+
 #ifndef SIZE
 #define SIZE 1024
 
