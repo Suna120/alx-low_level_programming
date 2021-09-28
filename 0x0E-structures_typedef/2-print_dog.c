d458630125f9a74618af3c577897f49d6690489f
SOnatti <90867768+SOnatti@users.noreply.github.com>
2021-09-28 20:55:48 +0200

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,22 +6,15 @@
  *
  * Return: Always 0.
  */
-
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-
-	if (d->name == NULL)
-		d->name = "(nil)";
-
-	if (d->owner == NULL)
-		d->owner = "(nil)";
-
-	printf("Name: %s\n", d->name);
-
-	printf("Age: %6f\n", d->age);
-
-	printf("Owner: %s\n", d->owner);
-
+if (d == NULL)
+return;
+if (d->name == NULL)
+d->name = "(nil)";
+if (d->owner == NULL)
+d->owner = "(nil)";
+printf("Name: %s\n", d->name);
+printf("Age: %6f\n", d->age);
+printf("Owner: %s\n", d->owner);
 }
