f6e069d2768148a1662e25252c4a711b32783a37
SOnatti <90867768+SOnatti@users.noreply.github.com>
2021-09-28 20:18:27 +0200

    Update 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
+else
+	return;
+}
