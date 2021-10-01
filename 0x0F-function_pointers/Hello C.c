1cb6c3813548b08fedfdcf0e5aa5e69bcee9e17a
SOnatti <s.moses@alustudent.com>
2021-10-02 00:32:44 +0200

    2index

---
+++ 0x0F-function_pointers/Hello C.c
@@ -0,0 +1,12 @@
+int main(){
+printf("Hello C programming\n");
+return 0;
+}
+
+int main(){
+int number;
+printf("enter a number:");
+scanf("enter a number:%d",number*number*number*number);
+return 0;
+
+}
