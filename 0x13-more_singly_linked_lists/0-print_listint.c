b6536d5f939e69a0c06efb151bb62d2a23a87c7d
SOnatti <90867768+SOnatti@users.noreply.github.com>
2021-10-12 20:48:14 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 /**
 *print_listint - prints all elements in listint_t list
@@ -9,11 +7,11 @@
 size_t print_listint(const listint_t *h)
 {
 size_t count= 0;
-while (h)
+while (h != NULL)
 {
 printf("%d\n", h->n);
-count++;
 h= h->next;
+count++;
 }
 return (count);
 }
