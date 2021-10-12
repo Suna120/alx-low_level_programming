827f22cb83e1f6af0d991d7276342583385eceec
SOnatti <s.moses@alustudent.com>
2021-10-12 22:22:31 +0200

    Nodes

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 /**
 *add_nodeint_end - adds a new node at the end of a listint_t list
@@ -9,19 +11,16 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *temp = malloc(sizeof(listint_t));
 listint_t *ptr = *head;
-
 if (temp == NULL)
 {
 return (NULL);
 }
 temp->n = n;
-
 if (*head == NULL)
 {
 *head = temp;
 return (*head);
 }
-
 while (ptr->next != NULL)
 {
 ptr = ptr->next;
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ *free_listint-Frees memory occupied by a list.
+ *@head:Pointer to the first node o the list.
+ *Return:Nothing.
+ */
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+
+if (head == NULL)
+{
+free(head);
+}
+else
+{
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}
+}
