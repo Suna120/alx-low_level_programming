c48df889a746eb0888b2d1877318e21855b23fec
SOnatti <90867768+SOnatti@users.noreply.github.com>
2021-10-12 19:02:16 +0200

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,14 +2,14 @@
 
 /**
 *listint_len - returns the number of elements in a linked listint_t list
-*@h: pointer to listint_len
+*@h: pointer to listint_t
 *Return: number of nodes.
 */
-
-size_t listint_len(const listint_t *h);
+size_t listint_len(const listint_t *h)
 {
-size_t count = 0;
-while (h)
+size_t count;
+count = 0;
+while (h != NULL)
 {
 count++;
 h = h->next;
