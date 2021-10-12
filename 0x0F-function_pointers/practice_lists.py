e9bb1063c6a8b80ff453079831341012b8681250
SOnatti <s.moses@alustudent.com>
2021-10-12 18:19:21 +0200

    Return

---
+++ 0x0F-function_pointers/practice_lists.py
@@ -0,0 +1,3 @@
+a = ["Programming 1","students","practice",1]
+b = ["programming",2021,"students","practice"]
+a == b
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+*print_listint - prints all elements of listint_t
+*@h: pointer to listint_t
+*Return: number of nodes.
+*/
+size_t listint_len(const listint_t *h);
+{
+size_t count= 0;
+while (h !=NULL)
+{
+count++;
+h= h->next;
+}
+return (count);
+}
