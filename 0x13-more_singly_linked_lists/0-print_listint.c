6bb31087415624da899aa4391a58c5150b63768d
SOnatti <90867768+SOnatti@users.noreply.github.com>
2021-10-12 20:39:20 +0200

    Update 0-print_listint.c
    Betty please work

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
-*print_listint - prints all elements of listint_t
+*print_listint - prints all elements in listint_t list
 *@h: pointer to listint_t
-*Return: number of nodes.
+*Return: count.
 */
 size_t print_listint(const listint_t *h)
 {
-size_t count= 0;
+size_t count;
+count= 0;
 while (h !=NULL)
 {
 printf("%d\n", h->n);
