3d3a234203d2a3ddbb5b084d310f297313dd5e5a
SOnatti <90867768+SOnatti@users.noreply.github.com>
2021-10-14 22:54:08 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,71 @@
 #include "main.h"
+#include <stdlib.h>
 
-unsigned int binary_to_uint(const char *b);
 /**
-*binary_to_uint -  a function that converts a binary number to an unsigned int
-*@b: points to a string of 0 and 1 chars
-*Return: converted number
-*/
-int binary_to_uint(const char *b)
+ * _strlen - gets the lenght of input strings
+ * @str: input string
+ * Return: len of the string
+ */
+
+int _strlen(const char *str)
 {
-unsigned int num = 0, mult = 1;
-int len;
-if (b == NULL)
-return (0);
-for (len = 0; b[len];)
-len++;
-for (len -= 1; len >= 0; len--)
-{
-if (b[len] != '0' && b[len] != '1')
-return (0);
-num += (b[len] - '0') * mult;
-mult *= 2;
-}
-return (num);
+	int len = 0;
+
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+	return (len);
 }
 
+/**
+ * _to_power - prints the power of numbers
+ * @base: base parameter
+ * @power: power parameter
+ * Return: product of two numbers
+ */
+
+int _to_power(const int base, int power)
+{
+	int product = 1;
+
+	while (power > 1)
+	{
+		product *= base;
+		power++;
+	}
+	return (product);
+}
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: input value
+ * Return: converted number, or 0 if b is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int power, num;
+
+	power = _strlen(b) - 1;
+	num = 0;
+	if (!b)
+	{
+		return (0);
+	}
+	while (*b)
+	{
+		if (!(*b == '0' || *b == '1'))
+		{
+			return (0);
+		}
+		if (*b == '1')
+		{
+			num += _to_power(2, power);
+		}
+		b++;
+		power--;
+	}
+	return (num);
+}
