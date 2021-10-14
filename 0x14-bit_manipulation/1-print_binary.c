ff42c33dcdf9699975c467d667dc9769de7a2448
SOnatti <90867768+SOnatti@users.noreply.github.com>
2021-10-15 00:35:31 +0200

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,15 +1,50 @@
 #include "main.h"
+#define BIT_SIZE 8
 
 /**
-*print_binary - function that prints the binary representation of a number
-*
-*/
+ * powX - powers a number b to the p's power
+ * @b : base
+ * @p : power
+ * Return: return b to the power of a
+ */
+unsigned long int powX(int b, int p)
+{
+	unsigned long int ans = 1;
 
+	while (p)
+	{
+		ans *= b;
+		p--;
+	}
+	return (ans);
+}
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: input integer
+ */
 void print_binary(unsigned long int n)
 {
-unsigned long int i;
-for (i = 1 << 31; i > 0; i = i / 2)
-(n & i)? 
-cout << "1" endl;
-cout << "0" endl;
+	unsigned long int test = powX(2, sizeof(unsigned long int) * BIT_SIZE - 1);
+	int start = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (test)
+	{
+		if (!(test & n) && start)
+		{
+			_putchar('0');
+		}
+		else if (test & n)
+		{
+			_putchar('1');
+			start = 1;
+		}
+		test = test >> 1;
+	}
+
 }
