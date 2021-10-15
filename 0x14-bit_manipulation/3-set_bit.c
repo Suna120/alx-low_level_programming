2131e5c9cc3d768fa2552f52ec22171bd1b5e968
SOnatti <90867768+SOnatti@users.noreply.github.com>
2021-10-16 01:35:13 +0200

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,1 +1,30 @@
 #include "main.h"
+#include "main.h"
+#define  BIT_SIZE 8
+
+unsigned long int powX(int b, int p)
+{
+	unsigned long int ans = 1;
+
+	while (p)
+	{
+		ans *= b;
+		p--;
+	}
+	return (ans);
+}
+
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int test;
+
+	if (index > sizeof(n) * BIT_SIZE - 1)
+		return (-1);
+
+
+	test = powX(2, index);
+	*n = *n | test;
+	return (1);
+
+}
