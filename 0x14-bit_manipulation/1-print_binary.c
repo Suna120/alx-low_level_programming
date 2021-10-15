0ce7626f9fca4504038a0e01a7c7c08da8a1dbe9
SOnatti <90867768+SOnatti@users.noreply.github.com>
2021-10-16 00:15:24 +0200

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,14 +9,13 @@
  */
 unsigned long int powX(int b, int p)
 {
-	unsigned long int ans = 1;
-
-	while (p)
-	{
-		ans *= b;
-		p--;
-	}
-	return (ans);
+unsigned long int ans = 1;
+while (p)
+{
+ans *= b;
+p--;
+}
+return (ans);
 }
 
 /**
@@ -25,26 +24,24 @@ unsigned long int powX(int b, int p)
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int test = powX(2, sizeof(unsigned long int) * BIT_SIZE - 1);
-	int start = 0;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (test)
-	{
-		if (!(test & n) && start)
-		{
-			_putchar('0');
-		}
-		else if (test & n)
-		{
-			_putchar('1');
-			start = 1;
-		}
-		test = test >> 1;
-	}
-
+unsigned long int test = powX(2, sizeof(unsigned long int) * BIT_SIZE - 1);
+int start = 0;
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+while (test)
+{
+if (!(test & n) && start)
+{
+_putchar('0');
+}
+else if (test & n)
+{
+_putchar('1');
+start = 1;
+}
+test = test >> 1;
+}
 }
