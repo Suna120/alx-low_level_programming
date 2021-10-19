495708b7dfad6d8d144905547d4312a63c74549f
Suna120 <92611358+Suna120@users.noreply.github.com>
2021-10-19 03:39:21 +0200

    Create 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*create_file - creates a file
+*@filename: the name of the file to create
+*@text_content: a NULL terminated string to write to the file
+*Returns: 1 on success, -1 on fail.
+*/
+int create_file(const char *filename, char *text_content)
+{
+register int fd, w, len = 0;
+if (!filename)
+return (-1);
+fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
+if (text_content)
+{
+while (text_content[len])
+len++;
+w = write(fd, text_content, len);
+if (w == -1)
+return (-1);
+}
+close(fd);
+return (1);
+}
