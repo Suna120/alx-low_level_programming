d8ef78f9f20472136bceeb99958bc5fb40b89674
Suna120 <92611358+Suna120@users.noreply.github.com>
2021-10-19 10:40:27 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,43 +6,37 @@
 *@filename: pointer to name of the file
 *@letters: the number of letters it should read and print
 *Return: the actual number of letters it could read and print
-*if the file can not be opened or read, return 0
-*if filename is NULL return 0
-*if write fails or does not write 
-*the expected amount of bytes, return 0
 */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 register int fd, r, w;
 char *buffer = NULL;
-
 if (!filename)
-	return (0);
+return (0);
 buffer = malloc(letters + 1);
 if (!buffer)
-	return (0);
+return (0);
 fd = open(filename, O_RDONLY);
-if (fd == -1)
+if (fd == -1)	
 {
-	free(buffer);
-	return (0);
-}
+free(buffer);
+return (0);
+}	
 r = read(fd, buffer, letters);
 if (r == -1)
 {
-	free(buffer);
-	close(fd);
-	return (0);
-}
+free(buffer);
+close(fd);
+return (0);
+}	
 buffer[letters] = '\0';
 w = write(STDOUT_FILENO, buffer, r);
 if (w == -1)
 {
-	free(buffer);
-	close(fd);
-	return (0);
-}
+free(buffer);
+close(fd);
+return (0);
+}	
 free(buffer);
 close(fd);
 return (w);
